f6b275d13ae8306523c601a939224075a5ecc808
aysenur <aysenurfortuna@gmail.com>
2024-02-12 21:17:57 +0300

    adding items

--- item/urls.py
+++ item/urls.py
@@ -7,5 +7,6 @@ app_name="item"
 
 
 urlpatterns = [
-    path('<int:pk>/', views.detail, name='detail')
+    path('<int:pk>/', views.detail, name='detail'),
+    path('new/', views.new, name="new"),
 ]
