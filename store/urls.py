2c435c61b6e7cd2e47d05deb1bba2a1258ecad3a
aysenur <aysenurfortuna@gmail.com>
2024-02-16 20:59:44 +0300

    add dashboard

--- store/urls.py
+++ store/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('items/', include('item.urls')),
     path('',  include('core.urls')),
     path('contact/', contact, name='contact'),
+    path('dashboard/', include('dashboard.urls')),
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
